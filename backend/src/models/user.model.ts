864a81ff6e298d34356d14b175e61777872c6c5f
Sachin Pal <sachinpal@Sachins-MacBook-Pro.local>
2025-06-02 17:58:54 +0530

    Backend - user model created

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import { compareValue, hashValue } from "../utils/bcrypt";
+
+export interface UserDocument extends mongoose.Document {
+  email: string;
+  password: string;
+  verified: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  comparePassword(val: string): Promise<boolean>;
+}
+
+const userSchema = new mongoose.Schema<UserDocument>(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    verified: { type: Boolean, default: false, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await hashValue(this.password);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (value: string) {
+  return compareValue(value, this.password);
+};
+
+const UserModel = mongoose.model("User", userSchema);
+export default UserModel;
