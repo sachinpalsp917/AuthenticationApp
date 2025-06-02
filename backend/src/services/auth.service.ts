49e64b69adc5ac9ca1c0e810092a1f1c8955172e
Sachin Pal <sachinpal@Sachins-MacBook-Pro.local>
2025-06-02 18:14:00 +0530

    Backend - created verificationCode model

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,3 +1,5 @@
+import UserModel from "../models/user.model";
+
 export type createAccountParams = {
   email: string;
   password: string;
@@ -6,7 +8,13 @@ export type createAccountParams = {
 
 export const createAccount = async (data: createAccountParams) => {
   //verify existing user doesn't exist
+  const existingUser = await UserModel.exists({ email: data.email });
+  if (existingUser) throw new Error("User already exists");
   //create user
+  const user = UserModel.create({
+    email: data.email,
+    password: data.password,
+  });
   //create verfication code
   //send verification mail
   //create session
