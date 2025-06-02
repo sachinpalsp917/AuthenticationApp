8918c859c0e5a2fb443e23dcc51b8d0ab15d6ed7
Sachin Pal <sachinpal@Sachins-MacBook-Pro.local>
2025-06-02 15:22:47 +0530

    Backend - connected Database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,17 @@
-import express from "express"
+import "dotenv/config";
+import express from "express";
+import connectToDatabase from "./config/db";
+import { NODE_ENV, PORT } from "./constants/env";
 
-const app = express()
+const app = express();
 
-app.get("/",(req,res)=>{
-    res.status(200).json({
-        message:"healthy"
-    })
-})
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "healthy",
+  });
+});
 
-app.listen(3000,()=>{
-    console.log("Server is listening on port 3000 in development enviourment");
-})
+app.listen(PORT, async () => {
+  console.log(`Server is listening on port ${PORT} in ${NODE_ENV} enviourment`);
+  await connectToDatabase();
+});
